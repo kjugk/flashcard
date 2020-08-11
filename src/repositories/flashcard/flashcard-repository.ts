78cc130171176c94a46e03d5c8399ee4d679bb70
Koji Uegaki <kjugkjk@gmail.com>
2020-08-11 20:47:05 +0900

    header 修正

--- src/repositories/flashcard/flashcard-repository.ts
+++ src/repositories/flashcard/flashcard-repository.ts
@@ -14,7 +14,7 @@ export class FlashcardRepository {
     const token = await getCognitoIdToken();
     return axios.create({
       baseURL: process.env.REACT_APP_API_BASE_URL,
-      headers: { Authorization: `Bearer ${token}` },
+      headers: { Authorization: token },
     });
   };
 
