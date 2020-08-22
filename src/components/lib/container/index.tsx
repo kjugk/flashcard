f077f2dc79403ea50671371ee1dd0b8f8aacc74a
Koji Uegaki <kjugkjk@gmail.com>
2020-08-22 16:33:28 +0900

    リストページのスタイル追加

--- src/components/lib/container/index.tsx
+++ src/components/lib/container/index.tsx
@@ -1,15 +1,21 @@
-import React, { FC } from "react";
+import React, { FC, CSSProperties } from "react";
 import styled from "styled-components";
 
 interface Props {
   tag?: "div" | "main";
+  style?: CSSProperties;
 }
 
-export const Container: FC<Props> = ({ tag = "div", children }) => {
-  return <StyledContainer as={tag}>{children}</StyledContainer>;
+export const Container: FC<Props> = ({ tag = "div", style, children }) => {
+  return (
+    <StyledContainer as={tag} style={style}>
+      {children}
+    </StyledContainer>
+  );
 };
 
 const StyledContainer = styled.div`
-  width: 768px;
+  background: transparent;
+  max-width: 768px;
   margin: 0 auto;
 `;
