e557965cac7d59af53d5c5ab9edd5c4cb54c6346
Koji Uegaki <kjugkjk@gmail.com>
2020-09-12 13:51:17 +0900

    最後のカードを表示

--- src/components/pages/flashcard-detail/qa-viewer/index.tsx
+++ src/components/pages/flashcard-detail/qa-viewer/index.tsx
@@ -20,6 +20,7 @@ export const QaViewer: FunctionComponent<Props> = ({ qaList }) => {
   const [inPageTransition, setInPageTransition] = useState(false);
   const [showAnswer, setShowAnswer] = useState(false);
   const [shuffling, setShuffling] = useState(false);
+  const [showLastPage, setShowLastPage] = useState(false);
   const [indexList, setIndexList] = useState<number[]>([]);
 
   const generateDefaultIndexList = () => {
@@ -61,31 +62,54 @@ export const QaViewer: FunctionComponent<Props> = ({ qaList }) => {
   return (
     <div>
       <CardWrapper>
-        <Card
-          inTransition={inPageTransition}
-          showAnswer={showAnswer}
-          onClick={() => setShowAnswer(!showAnswer)}
-        >
-          <CardContent>
-            <>
-              {!showAnswer && <CardDescription>問題</CardDescription>}
-              <pre>{currantQa.question}</pre>
-            </>
-          </CardContent>
+        {showLastPage && (
+          <Card showAnswer={false} inTransition={false}>
+            <CardContent>
+              <pre>終了です！ お疲れさまでした！</pre>
+              <Button
+                label="最初から"
+                size="s"
+                onClick={() => {
+                  setShowLastPage(false);
+                  setCurrentPage(1);
+                }}
+              />
+            </CardContent>
+          </Card>
+        )}
+        {!showLastPage && (
+          <Card
+            inTransition={inPageTransition}
+            showAnswer={showAnswer}
+            onClick={() => setShowAnswer(!showAnswer)}
+          >
+            <CardContent>
+              <>
+                {!showAnswer && <CardDescription>問題</CardDescription>}
+                <pre>{currantQa.question}</pre>
+              </>
+            </CardContent>
 
-          <CardContent className="answer">
-            <>
-              {showAnswer && <CardDescription>答え</CardDescription>}
-              <pre>{currantQa.answer}</pre>
-            </>
-          </CardContent>
-        </Card>
+            <CardContent className="answer">
+              <>
+                {showAnswer && <CardDescription>答え</CardDescription>}
+                <pre>{currantQa.answer}</pre>
+              </>
+            </CardContent>
+          </Card>
+        )}
       </CardWrapper>
 
       <Controller>
         <TextButton
           disabled={currentPage === 1}
-          onClick={() => changeCurrentPage(currentPage - 1)}
+          onClick={() => {
+            if (showLastPage) {
+              setShowLastPage(false);
+            } else {
+              changeCurrentPage(currentPage - 1);
+            }
+          }}
         >
           <ArrowBack style={{ fontSize: 40 }} />
         </TextButton>
@@ -93,8 +117,14 @@ export const QaViewer: FunctionComponent<Props> = ({ qaList }) => {
         <div className="pagenation">{`${currentPage}/${qaList.length}`}</div>
 
         <TextButton
-          disabled={currentPage === qaList.length}
-          onClick={() => changeCurrentPage(currentPage + 1)}
+          disabled={showLastPage}
+          onClick={() => {
+            if (currentPage === qaList.length) {
+              setShowLastPage(true);
+            } else {
+              changeCurrentPage(currentPage + 1);
+            }
+          }}
         >
           <ArrowFoward style={{ fontSize: 40 }} />
         </TextButton>
@@ -143,6 +173,7 @@ const CardContent = styled.div`
   border: 0.5px solid ${variables.colors.lightGrey};
   box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.15);
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
   font-size: ${variables.fontSize.xxl};
