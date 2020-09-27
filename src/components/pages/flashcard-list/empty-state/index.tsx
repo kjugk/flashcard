2b49e742e36e7470ff28b245c70b50dc6e3f8fb6
Koji Uegaki <kjugkjk@gmail.com>
2020-09-27 10:23:10 +0900

    ボタンのスタイルを修正

--- src/components/pages/flashcard-list/empty-state/index.tsx
+++ src/components/pages/flashcard-list/empty-state/index.tsx
@@ -1,24 +1,37 @@
 import React, { FC } from "react";
+import { variables } from "../../../../styles/variables";
 import { useHistory } from "react-router-dom";
-import { Box, Button } from "../../../lib";
+import { Button } from "../../../lib";
 import { Title } from "../../../lib/title";
+import Add from "@material-ui/icons/Add";
 
 export const EmptyState: FC = () => {
   const history = useHistory();
 
   return (
-    <Box style={{ textAlign: "center" }}>
+    <div
+      style={{
+        textAlign: "center",
+        background: variables.colors.lightBlue,
+        padding: "24px",
+        borderRadius: "6px",
+        color: variables.colors.white,
+      }}
+    >
       <Title
-        text="最初の暗記カードを作成しよう"
+        text="最初の問題集を作成しましょう"
         tag="h2"
-        size="xl"
+        size="m"
         style={{ marginBottom: "24px" }}
       />
       <Button
-        label="カード作成"
+        label="問題集を作成"
+        icon={<Add fontSize="inherit" />}
         size="xs"
+        outlined
+        style={{ margin: "0 auto" }}
         onClick={() => history.push("/flashcard-create")}
       />
-    </Box>
+    </div>
   );
 };
