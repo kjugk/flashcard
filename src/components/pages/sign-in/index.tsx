de69f6ca27c571def1e34635e9c24a69886463c8
Koji Uegaki <kjugkjk@gmail.com>
2020-10-05 23:38:51 +0900

    ログインページを作成

--- src/components/pages/sign-in/index.tsx
+++ src/components/pages/sign-in/index.tsx
@@ -1,18 +1,36 @@
 import React, { FunctionComponent } from "react";
+import styled from "styled-components";
 import { signInWithGoogle } from "../../../lib/cognito";
-import { SystemMessage } from "../../shared/system-message";
 import { useSignedInUserGuard } from "../../../global/current-user/current-user.store";
 import { useCurrentUserContext } from "../../../global/current-user/current-user.provider";
+import { ClosableHeader } from "../../shared/closable-header";
+import { Container } from "../../lib";
+import { useHistory } from "react-router-dom";
 
 export const SignInPage: FunctionComponent = () => {
   const { currentUserState } = useCurrentUserContext();
   useSignedInUserGuard(currentUserState);
+  const history = useHistory();
 
   return (
     <div>
-      <h1>ログイン</h1>
-      <button onClick={() => signInWithGoogle()}>Sign in with Google</button>
-      <SystemMessage />
+      <ClosableHeader title="ログイン" onClose={() => history.replace("/")} />
+      <Container tag="main" style={{ padding: "16px", textAlign: "center" }}>
+        <p>初めてご利用される方はログインが会員登録になります。</p>
+        <StyledButton onClick={signInWithGoogle}>
+          <img src="sign_in_with_google@2x.png" />
+        </StyledButton>
+      </Container>
     </div>
   );
 };
+
+const StyledButton = styled.button`
+  margin-top: 16px;
+  display: inline-block;
+  max-width: 191px;
+  background: transparent;
+  img {
+    max-width: 100%;
+  }
+`;
